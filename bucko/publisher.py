81e68087e74c7972a4c3616b5f918f685fcad85c
Ken Dreyer <kdreyer@redhat.com>
2017-01-04 13:26:51 -0700

    publisher: handle username in push SFTP URLs

--- bucko/publisher.py
+++ bucko/publisher.py
@@ -28,7 +28,7 @@ class Publisher(object):
         ssh = SSHClient()
         ssh.load_system_host_keys()
         # ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(url.netloc)
+        ssh.connect(url.netloc, username=url.username)
 
         sftp = ssh.open_sftp()
         sftp.put(file_, destfile)
