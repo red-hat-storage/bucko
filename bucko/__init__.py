1ada88eda85ca4372c38735d745eae59b6976732
Ken Dreyer <kdreyer@redhat.com>
2017-07-21 12:24:24 -0600

    interpolate koji config values with ConfigParser
    Now that we're using a regular ConfigParser object, we need to do the
    interpolation of %(branch)s within that, or we get parsing errors.

--- bucko/__init__.py
+++ bucko/__init__.py
@@ -113,16 +113,13 @@ def get_branch(compose):
 
 def build_container(repo_url, branch, configp):
     """ Build a container with Koji. """
-    kconf = dict(configp.items('koji'))
+    kconf = dict(configp.items('koji', vars={'branch': branch}))
     koji = KojiBuilder(hub=kconf['hub'],
                        web=kconf['web'],
                        krbservice=kconf['krbservice'])
     log.info('Building container at %s' % kconf['hub'])
-    # Interpolate %(branch)s for this compose.
-    scm = kconf['scm'] % {'branch': branch}
-    target = kconf['target'] % {'branch': branch}
-    task_id = koji.build_container(scm=scm,
-                                   target=target,
+    task_id = koji.build_container(scm=kconf['scm'],
+                                   target=kconf['target'],
                                    branch=branch,
                                    repos=[repo_url])
     # Show information to the console.
