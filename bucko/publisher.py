ab0b678fd8f770d1112cce270b33a5c1676bc66d
Ken Dreyer <kdreyer@redhat.com>
2018-05-11 14:32:02 -0600

    publisher: py3 support for urlparse import
    Try importing the py3 name by default, falling back to the py2 module name.

--- bucko/publisher.py
+++ bucko/publisher.py
@@ -1,5 +1,8 @@
 import posixpath
-from urlparse import urlparse
+try:
+    from urllib.parse import urlparse
+except ImportError:
+    from urlparse import urlparse
 import os
 from paramiko import SSHClient
 import shutil
