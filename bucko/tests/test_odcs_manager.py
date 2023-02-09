de328f4a7fa697391e047f15550ad9a1c8b15bb1
Ken Dreyer <kdreyer@redhat.com>
2023-02-09 14:26:06 -0500

    tests: call_args properties for py36
    Python 3.8 introduced the .args and .kwargs properties on .call_args
    (https://bugs.python.org/issue21269).
    
    Add backwards compatibility so we pass the tests on py36 where we must
    use the older numeric style to refer to the tuple elements.

--- bucko/tests/test_odcs_manager.py
+++ bucko/tests/test_odcs_manager.py
@@ -13,6 +13,9 @@ def test_generate(mock_wait, mock_request):
     assert result == 'https://example.com/example.repo'
 
     mock_request.assert_called_once()
+    # py36 compat for .call_args properties:
+    mock_request.call_args.args = mock_request.call_args[0]
+    mock_request.call_args.kwargs = mock_request.call_args[1]
     source = mock_request.call_args.args[0]
     assert isinstance(source, ComposeSourceTag)
     kwargs = mock_request.call_args.kwargs
