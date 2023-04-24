7ca65a9540cc36699b9ebd168d443a6527436ed9
Ken Dreyer <kdreyer@redhat.com>
2023-04-24 16:58:34 -0400

    tests: test Registry.find_realm_from_header() for red hat
    Add a unit test that asserts we can parse a WWW-Authenticate header that
    Red Hat's registry-proxy gives us.

--- bucko/tests/test_registry.py
+++ bucko/tests/test_registry.py
@@ -1,6 +1,18 @@
+import requests
 from bucko.registry import Registry
 
 
 def test_init():
     registry = Registry('http://registry.example.com')
     assert registry
+
+
+def test_find_realm_from_header_red_hat_registry_proxy():
+    registry = Registry('http://registry.example.com')
+    response = requests.Response()
+    # Red Hat's registry-proxy has a simple Bearer value.
+    response.headers = {
+      'WWW-Authenticate': 'Bearer realm="https://registry.example.com/v2/auth"'
+    }
+    realm = registry.find_realm_from_header(response)
+    assert realm == 'https://registry.example.com/v2/auth'
